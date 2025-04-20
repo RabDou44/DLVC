871fa2492ee6b627417835e99c12b61869292013
antal-spilyka <e11907555@student.tuwien.ac.at>
2025-04-20 11:12:11 +0200

    Added additional layers to CNN, model is done

--- assignments/assignment_1/assignment_1_code/models/cnn.py
+++ assignments/assignment_1/assignment_1_code/models/cnn.py
@@ -3,43 +3,57 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 
-
-
-
 class YourCNN(nn.Module):
     def __init__(self):
         super().__init__()
-        ## TODO implement
-        self.conv_1 = torch.nn.Conv2d(
-            3, 16, 5, stride=1,
-            padding='same', padding_mode='zeros')
-        self.bn_1 = torch.nn.BatchNorm2d(16)
-        self.pool_1 = torch.nn.MaxPool2d(2)
-        self.conv_2 = torch.nn.Conv2d(
-            16, 32, 5, stride=1,
-            padding='same', padding_mode='zeros')
-        self.bn_2 = torch.nn.BatchNorm2d(32)
-        self.pool_2 = torch.nn.MaxPool2d(2)
-        self.drop_1 = torch.nn.Dropout(0.5)
-        self.linear_1 = torch.nn.Linear(2048, 512)
-        self.bn_3 = torch.nn.BatchNorm1d(512)
-        self.linear_2 = torch.nn.Linear(512, 10)
+
+        # Convolutional block 1
+        self.conv_1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding='same')
+        self.bn_1 = nn.BatchNorm2d(16)
+        self.pool_1 = nn.MaxPool2d(2)
+
+        # Convolutional block 2
+        self.conv_2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding='same')
+        self.drop_1 = nn.Dropout2d(0.2)
+        self.pool_2 = nn.MaxPool2d(2)
+
+        # Convolutional block 3
+        self.conv_3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding='same')
+        self.bn_2 = nn.BatchNorm2d(64)
+        self.drop_2 = nn.Dropout2d(0.3)
+
+        # Fully connected layers
+        self.linear_1 = nn.Linear(4096, 256)  # Adjust this if input image size changes
+        self.bn_3 = nn.BatchNorm1d(256)
+        self.drop_3 = nn.Dropout(0.5)
+
+        self.linear_2 = nn.Linear(256, 64)
+        self.linear_3 = nn.Linear(64, 10)
 
     def forward(self, x):
-        ## TODO implement
         x = self.conv_1(x)
-        x = self.bn_1(x)
-        x = torch.nn.functional.relu(x)
+        x = F.relu(x)
         x = self.pool_1(x)
-        x = self.conv_2(x)
-        x = self.bn_2(x)
-        x = torch.nn.functional.relu(x)
-        x = self.pool_2(x)
-        x = torch.flatten(x, start_dim=1)
-        x = self.drop_1(x)
-        x = self.linear_1(x)
-        x = self.bn_3(x)
-        x = torch.nn.functional.relu(x)
-        return self.linear_2(x)
+        x = self.bn_1(x)
 
-    
+        x = self.conv_2(x)
+        x = F.relu(x)
+        x = self.drop_1(x)
+        x = self.pool_2(x)
+
+        x = self.conv_3(x)
+        x = F.relu(x)
+        x = self.drop_2(x)
+        x = self.bn_2(x)
+
+        x = torch.flatten(x, start_dim=1)
+
+        x = self.linear_1(x)
+        x = F.relu(x)
+        x = self.drop_3(x)
+        x = self.bn_3(x)
+
+        x = self.linear_2(x)
+        x = F.relu(x)
+
+        return self.linear_3(x)
